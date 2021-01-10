38ca29ea883d4aed918ee0498aa12d9d1e401eda
gabrielheiwa <heiwa.gabriel@gmail.com>
2021-01-10 08:08:08 -0300

    add: music controller in the routes

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,12 @@
 import { Router, Request, Response } from "express";
 import multer from "./config/multer.conf"
+import Music_Controller from "./controller/Music_Controller";
 
 const routes = Router();
 
-routes.post("/music_upload", multer.single("music"), (req: Request, res: Response) => {
-    res.sendStatus(200);
-});
+routes.post("/music_upload",
+    multer.single("music"),
+    Music_Controller.music_upload
+);
 
 export default routes;
