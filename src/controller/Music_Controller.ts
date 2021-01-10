470af0e24ba1e00641280687b108f264f3ff8079
gabrielheiwa <heiwa.gabriel@gmail.com>
2021-01-10 08:03:23 -0300

    add: music upload route

---
+++ src/controller/Music_Controller.ts
@@ -0,0 +1,6 @@
+import { Request, Response } from "express";
+export default {
+    async music_upload(req: Request, res: Response) {
+        res.sendStatus(200).send("upload feito com sucesso.");
+    },
+}
--- src/routes.ts
+++ src/routes.ts
@@ -1,8 +1,9 @@
 import { Router, Request, Response } from "express";
+import multer from "./config/multer.conf"
 
 const routes = Router();
 
-routes.get("/", (req: Request, res: Response) => {
+routes.post("/music_upload", multer.single("music"), (req: Request, res: Response) => {
     res.sendStatus(200);
 });
 
