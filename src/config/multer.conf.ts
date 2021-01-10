853517db462a9087f231e82070d7a124c7800bdf
gabrielheiwa <heiwa.gabriel@gmail.com>
2021-01-10 07:51:20 -0300

    add: file multer config

---
+++ src/config/multer.conf.ts
@@ -0,0 +1,14 @@
+import multer, { diskStorage } from "multer";
+import path from "path";
+
+export default multer({
+    dest: path.resolve(__dirname, "..", "database", "music"),
+    storage: diskStorage({
+        destination: (req, file, callback) => {
+            callback(null, path.resolve(__dirname, "..", "database", "music"))
+        },
+        filename: (req, file, callback) => {
+            callback(null, `${file.originalname}-${Date.now()}`)
+        },
+    })
+});
